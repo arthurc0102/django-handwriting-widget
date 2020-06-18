1f32b89c478c3a4317310dea33f07df3200d0321
Arthur Chang <arthurc0102@gmail.com>
2020-06-18 12:12:33 +0800

    style(flake8): fix lint error

--- e_signatures/models.py
+++ e_signatures/models.py
@@ -6,7 +6,11 @@ from .services import signature_image_path
 class Signature(models.Model):
     name = models.CharField(max_length=10, unique=True)
     image = models.ImageField(upload_to=signature_image_path)
-    image_no_required = models.ImageField(upload_to=signature_image_path, null=True, blank=True)
+    image_no_required = models.ImageField(
+        upload_to=signature_image_path,
+        null=True,
+        blank=True,
+    )
     create_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
